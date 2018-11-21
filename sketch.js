cb9d270dfc03cc557c02422db8ddd39b2537f5a1
kenny <kenny@pinkclicks.com>
2018-11-21 17:38:20 +0700

    add viewport for mobile; add swipe controls

--- sketch.js
+++ sketch.js
@@ -526,5 +526,57 @@ document.addEventListener('DOMContentLoaded', () => {
 			game.input(controls[key]);
 		}
 	});
+
+	document.addEventListener('touchstart', handleTouchStart, false);        
+	document.addEventListener('touchmove', handleTouchMove, false);
+
+	var xDown = null;                                                        
+	var yDown = null;
+
+	function getTouches(evt) {
+	  return evt.touches ||             // browser API
+			 evt.originalEvent.touches; // jQuery
+	}                                                     
+
+	function handleTouchStart(evt) {
+		const firstTouch = getTouches(evt)[0];                                      
+		xDown = firstTouch.clientX;                                      
+		yDown = firstTouch.clientY;                                      
+	};                                                
+
+	function handleTouchMove(evt) {
+		evt.preventDefault();
+		if ( ! xDown || ! yDown ) {
+			return;
+		}
+
+		var xUp = evt.touches[0].clientX;                                    
+		var yUp = evt.touches[0].clientY;
+
+		var xDiff = xDown - xUp;
+		var yDiff = yDown - yUp;
+
+		if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+			if ( xDiff > 0 ) {
+				/* left swipe */ 
+				game.input('left');
+			} else {
+				/* right swipe */
+				game.input('right');
+			}                       
+		} else {
+			if ( yDiff > 0 ) {
+				/* up swipe */ 
+				game.input('rotate');
+			} else { 
+				/* down swipe */
+				game.input('down');
+			}                                                                 
+		}
+		/* reset values */
+		xDown = null;
+		yDown = null;                                             
+	};
+
 });
 
